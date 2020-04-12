329a535e6e02260f224ef7a593810a71e775cc73
Arton Mirakaj <arton.mirakaj@commonbond.co>
2020-04-12 12:08:49 -0400

    directory state into redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,6 +3,7 @@ import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryreducer from './directory/directory.reducer';
 
 const persistConfig = {
   key: 'root',
@@ -12,7 +13,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryreducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);

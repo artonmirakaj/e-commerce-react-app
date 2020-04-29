0dcd61d2d319d27586bf8afbe689d78a259ed657
Arton Mirakaj <arton.mirakaj@commonbond.co>
2020-04-29 18:28:50 -0400

    moved data to firebase

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
